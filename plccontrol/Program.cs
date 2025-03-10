5a9904b8b07678c1b04acc80a4f2e37a529fc4cc
hyeyeoung <rlagpdud0524@gmail.com>
2025-03-10 11:38:03 +0900

    rabbit mq 연결 및 plc 제어 버튼 이벤트 등록

--- plccontrol/Program.cs
+++ plccontrol/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using RabbitMQ.Client;
+using plccontrol;
+
+namespace receiveSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rabbitmq rbmq = rabbitmq.Instance(); // 객체 생성 (+mq 연결)
+            rbmq.plccontrol += plcControl; 
+            
+            Thread readmessage = new Thread(new ThreadStart(rbmq.readONOFF));
+            readmessage.Start();
+
+            Thread.Sleep(-1);
+        }
+        // 버튼 이벤트 등록
+        static void plcControl(object sender, EventArgs e)
+        {
+            string[] x = (string[])sender;
+            Console.WriteLine($"이벤트 발생 : {x[0]}");
+        }
+    }
+}
---
+++ plccontrol/rabbitmq.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace plccontrol
+{
+    internal class rabbitmq
+    {
+        /// <summary>
+        /// 통신하기
+        /// </summary>
+        ConnectionFactory factory;
+        IChannel channel;
+        IConnection connection;
+        static rabbitmq staticrabbitmq;
+        public event EventHandler plccontrol; // plc 이벤트 컨트롤 등록
+
+        public static rabbitmq Instance()
+        {
+            if (staticrabbitmq == null)
+                staticrabbitmq = new rabbitmq();
+            return staticrabbitmq;
+        }
+        private async Task _connect()
+        {
+            try
+            {
+                factory = new ConnectionFactory
+                {
+                    HostName = "211.187.0.113",
+                    UserName = "guest",
+                    Password = "guest",
+                    Port = 5672
+                };
+                this.connection = await this.factory.CreateConnectionAsync();
+                this.channel = await this.connection.CreateChannelAsync();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        // 큐 메세지 기다리는 함수
+        public async void readONOFF()
+        {
+            try
+            {
+                // rabbit mq 메세지 sub하기
+                await this._connect();
+                QueueDeclareOk queueDeclareResult = await this.channel.QueueDeclareAsync();
+                string queuename = queueDeclareResult.QueueName;
+                await this.channel.QueueBindAsync(queue: queuename, exchange: "control", routingKey: string.Empty);
+                var consumer = new AsyncEventingBasicConsumer(this.channel);
+                
+                consumer.ReceivedAsync += (model, ea) =>
+                {
+                    byte[] body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    
+                    Console.WriteLine($"message : {message}");
+                    string[] switchData = message.Split(", ");
+
+                    // 기계 off시 수행
+                    plccontrol?.Invoke(switchData, EventArgs.Empty);
+                    
+                    return Task.CompletedTask;
+                };
+                await this.channel.BasicConsumeAsync(queuename, autoAck: true, consumer: consumer);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        
+    }
+}
